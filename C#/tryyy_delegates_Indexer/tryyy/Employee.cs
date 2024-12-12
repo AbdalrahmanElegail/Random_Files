1007d22149bb43d455bfd7044d2ed644935d68c8
Abdalrahman <Abdalrahmanelegail@gmail.com>
2024-12-13 01:01:10 +0200

    added an indexer to the program

---
+++ C#/tryyy_delegates_Indexer/tryyy/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace tryyy
+{
+
+    internal class People
+    {
+        public People[]? PeopleList { get; private set; }
+        public void SetPeopleList(People[] peopleList) { PeopleList = peopleList; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Gender { get; set; }
+        public double Sales { get; set; }
+
+        public People this[int id]
+        {
+            get
+            {
+                if (PeopleList == null || id <= 0 || id > PeopleList.Length)
+                    throw new IndexOutOfRangeException("Invalid employee ID.");
+                return PeopleList[id - 1];
+            }
+            set
+            {
+                if (PeopleList == null || id <= 0 || id > PeopleList.Length)
+                    throw new IndexOutOfRangeException("Invalid employee ID.");
+                PeopleList[id - 1] = value;
+            }
+        }
+        public override string ToString() => $"Id: {Id} | Name: {Name} | Gender: {Gender} | Sales: {Sales} ";
+    }
+}
